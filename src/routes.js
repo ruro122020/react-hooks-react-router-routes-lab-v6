e7ec683ad0f788d2b376a70a4d62fadae5a8ecb1
Ruth <ruro122020@gmail.com>
2023-10-28 19:42:30 -0400

    setup react-router-dom

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,18 @@
 
 
 const routes = [
-   
+   {
+    route:'/',
+   },
+   {
+    route:'/directors',
+   },
+   {
+    route:'/actors'
+   },
+   {
+    route:'/movie/:id'
+   }
   ];
 
 export default routes;
