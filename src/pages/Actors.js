048f5e7ece92caca983f6499bc968279978a318a
Ruth <ruro122020@gmail.com>
2023-10-29 13:40:07 -0400

    actors page complete

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,13 +1,37 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Actors() {
+  const [actors, setActors] = useState({})
+
+  useEffect(()=>{
+    fetch('http://localhost:4000/actors')
+    .then(res => res.json())
+    .then(actorsData => setActors(actorsData))
+  }, [])
+
+  if(!actors.length) return <h1>Loading ...</h1>
+
+  const displayActors = actors.map(actor => {
+    return (
+      <article key={actor.id}>
+        <h2>{actor.name}</h2>
+        <ul>
+          {actor.movies.map(movie => <li key={movie}>{movie}</li>)}
+        </ul>
+      </article>
+    )
+  })
   return (
     <>
       <header>
         {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
         {/* Actor info here! */}
+        <h1>Actors Page</h1>
+        {displayActors}
       </main>
     </>
   );
